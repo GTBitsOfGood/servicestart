97b5f10577772cee0e5f6ca493963d00a34481d5
Renato Dell'Osso <rddellosso4@gmail.com>
2026-01-21 17:12:26 -0500

    Add prettier plugin for ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,14 @@ import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import reactHooks from 'eslint-plugin-react-hooks';
 import vitest from '@vitest/eslint-plugin';
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.recommended,
   reactHooks.configs.flat.recommended,
   vitest.configs.recommended,
+  eslintConfigPrettier,
   {
     ignores: ['node_modules/**', '.next/**'],
   },
