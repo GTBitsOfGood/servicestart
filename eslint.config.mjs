ae77b6d5c2c16647e3a952ca1fba345057f73762
Renato Dell'Osso <rddellosso4@gmail.com>
2026-01-21 19:34:47 -0500

    Fix formatting, add lockfile to ignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import reactHooks from 'eslint-plugin-react-hooks';
 import vitest from '@vitest/eslint-plugin';
-import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
 
 export default defineConfig(
   eslint.configs.recommended,
