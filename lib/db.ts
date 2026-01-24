a438961f10516803eb068723aa26db0ba13277c0
Renato Dell'Osso <rddellosso4@gmail.com>
2026-01-23 22:03:19 -0500

    Fix build

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,8 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { relations, schema } from "./schema";
 
-export default drizzle(process.env.DB_URL!, {
+// The string is odd, but it fixes the build. Apparently Drizzle needs something there at build time.
+export default drizzle(process.env.DB_URL ?? "", {
   relations,
   schema,
   casing: "snake_case",
