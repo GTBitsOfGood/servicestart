28ef2c682bd34010b34f3385ce6ecc8f126fe605
Renato Dell'Osso <rddellosso4@gmail.com>
2026-01-31 23:44:54 -0500

    Add error handling for ping route

--- app/api/ping/route.ts
+++ app/api/ping/route.ts
@@ -2,9 +2,14 @@ import db from "@/lib/db";
 
 export async function GET() {
   let dbConnected: boolean = false;
-  await db.execute(`SELECT 1`).then(() => {
-    dbConnected = true;
-  });
+  await db
+    .execute(`SELECT 1`)
+    .then(() => {
+      dbConnected = true;
+    })
+    .catch(() => {
+      dbConnected = false;
+    });
 
   const msg = `DB connected: ${dbConnected}. Branch: ${process.env.NEXT_PUBLIC_BRANCH_NAME}`;
 
