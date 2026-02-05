8e43524a03073b543f34b90e687778b81b00202e
Renato Dell'Osso <rddellosso4@gmail.com>
2026-02-05 08:46:11 -0500

    Remove testing code from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,7 @@
-"use client";
-
-import api from "@/lib/api";
-import { useEffect } from "react";
-
-// export const metadata = {
-//   title: "App Router",
-// };
+export const metadata = {
+  title: "App Router",
+};
 
 export default function Page() {
-  useEffect(() => {
-    api.ping.$get().then(async (response) => {
-      console.log("Ping response:", await response.text());
-    });
-  });
-
   return <h1>App Router</h1>;
 }
