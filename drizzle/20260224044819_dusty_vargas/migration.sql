c0cff6f7ac66a16474817d0009175e8322bc0485
Renato Dell'Osso <rddellosso4@gmail.com>
2026-02-24 09:12:40 -0500

    Fix migrations

--- drizzle/20260224044819_dusty_vargas/migration.sql
+++
@@ -1,6 +0,0 @@
-ALTER TYPE "organization_config_key" ADD VALUE 'primary_color';--> statement-breakpoint
-ALTER TYPE "organization_config_key" ADD VALUE 'secondary_color';--> statement-breakpoint
-ALTER TYPE "organization_config_key" ADD VALUE 'tagline';--> statement-breakpoint
-ALTER TABLE "shifts" ADD COLUMN "eventId" text NOT NULL;--> statement-breakpoint
-CREATE INDEX "shift_eventId_idx" ON "shifts" ("eventId");--> statement-breakpoint
-ALTER TABLE "shifts" ADD CONSTRAINT "shifts_eventId_events_id_fkey" FOREIGN KEY ("eventId") REFERENCES "events"("id") ON DELETE CASCADE;
