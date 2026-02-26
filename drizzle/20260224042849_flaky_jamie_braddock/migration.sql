aed33e2186427e7b96807bf2b1fab5b3340beec6
Renato Dell'Osso <rddellosso4@gmail.com>
2026-02-26 08:52:12 -0500

    Fix migrations

--- drizzle/20260224042849_flaky_jamie_braddock/migration.sql
+++ drizzle/20260224042849_flaky_jamie_braddock/migration.sql
@@ -1,3 +1,4 @@
-ALTER TABLE "shifts" ADD COLUMN "eventId" text NOT NULL;--> statement-breakpoint
-CREATE INDEX "shift_eventId_idx" ON "shifts" ("eventId");--> statement-breakpoint
+ALTER TABLE "shifts" ADD COLUMN IF NOT EXISTS "eventId" text NOT NULL;--> statement-breakpoint
+CREATE INDEX IF NOT EXISTS "shift_eventId_idx" ON "shifts" ("eventId");--> statement-breakpoint
+ALTER TABLE "shifts" DROP CONSTRAINT IF EXISTS "shifts_eventId_events_id_fkey";--> statement-breakpoint
 ALTER TABLE "shifts" ADD CONSTRAINT "shifts_eventId_events_id_fkey" FOREIGN KEY ("eventId") REFERENCES "events"("id") ON DELETE CASCADE;
