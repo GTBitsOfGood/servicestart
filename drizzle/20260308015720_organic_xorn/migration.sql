69ecebc582acab526df55ec98dbdb3ea698158fc
cindyz0 <cindy.zhu1@gmail.com>
2026-03-07 21:03:46 -0500

    comment out duplicates

--- drizzle/20260308015720_organic_xorn/migration.sql
+++ drizzle/20260308015720_organic_xorn/migration.sql
@@ -1,3 +1,3 @@
-ALTER TYPE "organization_config_key" ADD VALUE 'navbar_variant' BEFORE 'members_page_enabled';--> statement-breakpoint
-ALTER TYPE "organization_config_key" ADD VALUE 'navbar_color' BEFORE 'members_page_enabled';--> statement-breakpoint
-ALTER TYPE "organization_config_key" ADD VALUE 'logo_url';
+-- ALTER TYPE "organization_config_key" ADD VALUE 'navbar_variant' BEFORE 'members_page_enabled';--> statement-breakpoint
+-- ALTER TYPE "organization_config_key" ADD VALUE 'navbar_color' BEFORE 'members_page_enabled';--> statement-breakpoint
+-- ALTER TYPE "organization_config_key" ADD VALUE 'logo_url';
