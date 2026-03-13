bedd669a57daff31db2f526bf49bb3e729bb2bbe
Renato Dell'Osso <rddellosso4@gmail.com>
2026-03-13 17:47:14 -0400

    Fix migrations

---
+++ drizzle/20260313214609_gifted_turbo/migration.sql
@@ -0,0 +1,8 @@
+-- ALTER TABLE "announcements" RENAME COLUMN "title" TO "name";--> statement-breakpoint
+-- ALTER TABLE "announcements" RENAME COLUMN "body" TO "content";--> statement-breakpoint
+-- ALTER TABLE "announcements" ADD COLUMN "subject" text NOT NULL;--> statement-breakpoint
+-- ALTER TABLE "announcements" ADD COLUMN "template" boolean DEFAULT false;--> statement-breakpoint
+ALTER TABLE "events" ADD COLUMN "published_at" timestamp;--> statement-breakpoint
+ALTER TABLE "events" ADD COLUMN "published_by_id" text;--> statement-breakpoint
+-- ALTER TABLE "announcements" ALTER COLUMN "content" SET DATA TYPE jsonb USING "content"::jsonb;--> statement-breakpoint
+ALTER TABLE "events" ADD CONSTRAINT "events_published_by_id_users_id_fkey" FOREIGN KEY ("published_by_id") REFERENCES "users"("id") ON DELETE CASCADE;
