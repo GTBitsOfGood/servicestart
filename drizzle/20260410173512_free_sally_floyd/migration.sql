9e73d0eee41fcbf0c57fc31915d83d98666fa50b
Lindsay <lindsayhwang@gmail.com>
2026-04-12 08:31:19 -0400

    removed dupe migrations

--- drizzle/20260410173512_free_sally_floyd/migration.sql
+++
@@ -1,2 +0,0 @@
-ALTER TYPE "organization_config_key" ADD VALUE 'mobile_navbar_show_icons';--> statement-breakpoint
-ALTER TYPE "organization_config_key" ADD VALUE 'mobile_navbar_profile_orientation';
