62c9c4fd86d47649e88efac8d2fd891cd86bbab6
Rohan Godha <git@rohangodha.com>
2026-04-12 18:35:23 -0400

    make join requests ui match figma

--- app/joinrequeststatus/components/icons/ApprovedStatusIcon.tsx
+++
@@ -1,8 +0,0 @@
-import { Check } from "@phosphor-icons/react/ssr";
-import type { ComponentProps } from "react";
-
-type ApprovedStatusIconProps = ComponentProps<typeof Check>;
-
-export default function ApprovedStatusIcon(props: ApprovedStatusIconProps) {
-  return <Check aria-hidden="true" size={20} weight="bold" {...props} />;
-}
